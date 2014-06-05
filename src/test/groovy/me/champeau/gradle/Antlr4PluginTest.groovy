20a672fc82be23078ac41f2cfc73959c5af7f03f
Stephen Gaito <stephen@perceptisys.co.uk>
2014-06-05 15:32:28 +0100

    added tests

--- src/test/groovy/me/champeau/gradle/Antlr4PluginTest.groovy
+++ src/test/groovy/me/champeau/gradle/Antlr4PluginTest.groovy
@@ -16,4 +16,39 @@ class Antlr4PluginTest {
         assert task.source == project.file("src/main/antlr4")
         assert task.output == project.file("$project.buildDir/generated-src")
     }
+
+    @Test
+    void defaultDependency() {
+        Project project = ProjectBuilder.builder().build()
+        project.with {
+            apply plugin: 'antlr4'
+        }
+        project.evaluate()
+
+        def antlr4config = project.getConfigurations().getByName('antlr4')
+        def antlr4dependencies = antlr4config.getDependencies().toArray()
+        assert antlr4dependencies.size() == 1
+        assert antlr4dependencies[0].getGroup() == 'org.antlr'
+        assert antlr4dependencies[0].getName() == 'antlr4'
+        assert antlr4dependencies[0].getVersion() == '4.2.2'
+    }
+
+    @Test
+    void usersDependency() {
+        Project project = ProjectBuilder.builder().build()
+        project.with {
+            apply plugin: 'antlr4'
+            dependencies {
+                antlr4 'org.antlr:antlr4:4.2.3-dev'
+            }
+        }
+        project.evaluate()
+
+        def antlr4config = project.getConfigurations().getByName('antlr4')
+        def antlr4dependencies = antlr4config.getDependencies().toArray()
+        assert antlr4dependencies.size() == 1
+        assert antlr4dependencies[0].getGroup() == 'org.antlr'
+        assert antlr4dependencies[0].getName() == 'antlr4'
+        assert antlr4dependencies[0].getVersion() == '4.2.3-dev'
+    }
 }
